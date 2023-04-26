daff804d30886cd7c63a0afb1b1f0c78fde402d0
marikomands <marikomands@gmail.com>
2023-04-26 11:32:23 +0100

    created components

---
+++ src/components/CurrentWeatherDetails.js
@@ -0,0 +1,7 @@
+// import React from "react";
+
+// const ImageList = ({ weatherData }) => {
+//   const currentWeatherData = weatherData.map((data) => {
+//     return <div><{}/div>;
+//   });
+// };
