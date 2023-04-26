8e7f0f8a6466a45b1c2c949d6435315aa85dc33a
marikomands <marikomands@gmail.com>
2023-04-26 11:22:08 +0100

    Created SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SearchBar = () => {
-  return <div>SearchBar</div>;
+const SearchBar = ({ onSubmit }) => {
+  const [city, setCity] = useState("");
+  console.log(city);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("🚀 ~ onFormSubmit ~ onFormSubmit");
+    onSubmit(city);
+  };
+
+  return (
+    <div className="containerWrapper">
+      <form className="formSubmit" onSubmit={onFormSubmit}>
+        <label className="fontLabel">Weather Search</label>
+        <input
+          className="input"
+          value={city}
+          type="text"
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default SearchBar;
