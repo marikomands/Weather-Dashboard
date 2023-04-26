6366369612d158be0401313f8137772085fc0c38
marikomands <marikomands@gmail.com>
2023-04-26 11:22:50 +0100

    Created App compoent

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import CurrentWeather from "./CurrentWeather";
+import React from "react";
+
+const App = () => {
+  return <CurrentWeather />;
+};
+
+export default App;
+
+// import React, { Component } from "react";
+// import unsplash from "../api/unsplash";
+// import SearchBar from "./SearchBar";
+// import ImageList from "./ImageList";
+
+// class App extends Component {
+//   state = { images: [] };
+
+//   onSearchSubmit = async (term) => {
+//     const response = await unsplash.get("/search/photos", {
+//       params: { query: term },
+//     });
+//     console.log("🚀 ~ App ~ onSearchSubmit= ~ response", response);
+//     // console.log("🚀 ~ file: App.js ~ line 15 ~ App ~ response", response);
+//     // .then((response) => {
+//     //   console.log(
+//     //     "🚀 ~ file: App.js ~ line 14 ~ App ~ response",
+//     //     response.data.results
+//     //   );
+//     // });
+//     this.setState({ images: response.data.results });
+//   };
+
+//   render() {
+//     return (
+//       <div className="ui container" style={{ marginTop: "10px" }}>
+//         <SearchBar onSubmit={this.onSearchSubmit} />
+//         <ImageList images={this.state.images} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
