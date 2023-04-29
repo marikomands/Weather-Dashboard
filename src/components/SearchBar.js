bc13d392a202bd0ea25f226f78d158fe0d1044b4
marikomands <marikomands@gmail.com>
2023-04-29 21:25:38 +0100

    Removed Form tag

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,25 +4,26 @@ const SearchBar = ({ onSubmit }) => {
   const [city, setCity] = useState("");
   console.log(city);
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    console.log("🚀 ~ onFormSubmit ~ onFormSubmit");
-    onSubmit(city);
-  };
+  // const onFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   console.log("🚀 ~ onFormSubmit ~ onFormSubmit");
+  // onSubmit(city);
+  // };
 
   return (
     <div className="containerWrapper">
-      <form className="formSubmit" onSubmit={onFormSubmit}>
-        <label className="fontLabel">Weather Search</label>
-        <input
-          className="input"
-          value={city}
-          type="text"
-          onChange={(e) => {
-            setCity(e.target.value);
-          }}
-        />
-      </form>
+      {/* <form className="formSubmit" onSubmit={onFormSubmit}> */}
+      <label className="fontLabel">City Name</label>
+      <input
+        className="input"
+        value={city}
+        type="text"
+        onChange={(e) => {
+          setCity(e.target.value);
+          onSubmit(city);
+        }}
+      />
+      {/* </form> */}
     </div>
   );
 };
