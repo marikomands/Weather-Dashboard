a2bcc54e3f4e24837cc26087fb711f71d9aabaea
marikomands <marikomands@gmail.com>
2023-05-10 16:21:24 +0100

    added header

--- src/components/CurrentWeatherDetails.js
+++
@@ -1,7 +0,0 @@
-// import React from "react";
-
-// const ImageList = ({ weatherData }) => {
-//   const currentWeatherData = weatherData.map((data) => {
-//     return <div><{}/div>;
-//   });
-// };
