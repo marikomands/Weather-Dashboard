6a05e2bce475a7f60d684346a5dc4fdd692c7076
marikomands <marikomands@gmail.com>
2023-05-17 14:10:05 +0100

    updated Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-import React from "react";
+import react, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+// import "./Header.css";
 
-const Header = () => {
-  return <div></div>;
+const Header = ({ city }) => {
+  return (
+    <div className="header">
+      <Link
+        className="linkHeader"
+        to={`/seven-days/weather-forecast/${encodeURIComponent(city)}`}
+      >
+        3 Days Forecast
+      </Link>
+    </div>
+  );
 };
 
 export default Header;
