80a15311bae4e89681af937d395a0a12695155d5
marikomands <marikomands@gmail.com>
2023-05-22 13:34:37 +0100

    changed from Header to ForecastLink component

---
+++ src/components/ForecastLink.js
@@ -0,0 +1,18 @@
+import react, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+// import "./Header.css";
+
+const ForecastLink = ({ city }) => {
+  return (
+    <div className="header">
+      <Link
+        className="linkHeader"
+        to={`/seven-days/weather-forecast/${encodeURIComponent(city)}`}
+      >
+        3 Days Forecast
+      </Link>
+    </div>
+  );
+};
+
+export default ForecastLink;
